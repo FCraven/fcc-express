86e98b9ae5b03bdd6d6d74e16897de017d147c47
FCraven <craven.francis@gmail.com>
2021-05-24 15:08:30 -0400

    exercise 4 typo

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-app.use(express.static(path.join(__dirname, './public')))
+app.use(express.static('/public'))
 
 if (!process.env.DISABLE_XORIGIN) {
   app.use(function(req, res, next) {
