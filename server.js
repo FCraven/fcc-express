bb8a577a902f52a143da431f4c6d997fd0acf0bd
FCraven <craven.francis@gmail.com>
2021-05-24 15:11:59 -0400

    exercise 4 - error fixed

--- server.js
+++ server.js
@@ -9,20 +9,20 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-app.use(express.static('/public'))
-
 if (!process.env.DISABLE_XORIGIN) {
   app.use(function(req, res, next) {
     var allowedOrigins = ['https://narrow-plane.gomix.me', 'https://www.freecodecamp.com'];
     var origin = req.headers.origin || '*';
     if(!process.env.XORIG_RESTRICT || allowedOrigins.indexOf(origin) > -1){
-         console.log(origin);
-         res.setHeader('Access-Control-Allow-Origin', origin);
-         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      console.log(origin);
+      res.setHeader('Access-Control-Allow-Origin', origin);
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     }
     next();
   });
 }
+
+app.use(express.static('public'))
 
 app.get('/', (req, res)=> {
   res.sendFile(path.join(__dirname, './views/index.html'))
